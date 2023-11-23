affca17e011cac9b9faf77772ccea5ec54337a09
gabzanj <gg.gabzanj@gmail.com>
2023-11-22 23:08:12 -0300

    add files python_techniques

---
+++ linkedin_learning/python_techniques/02_builtin_functions.py
@@ -0,0 +1,27 @@
+def functions():
+    list = [2, 5, 12, 7, 8, 10]
+
+    # TODO: 'any' returns True if any value in the list is true
+    print(any(list))
+
+    # TODO: 'all' returns True if all values in the list are true
+    print(all(list))
+
+    # TODO: minimum and maximum values
+    print(f'Minimum: {min(list)}')
+    print(f'Maximum: {max(list)}')
+
+    # TODO: sum of values
+    print(f'Sum: {sum(list)}')
+
+def iterator_functions():
+    days_ptbr = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+    days_en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+
+
+def main():
+    functions()
+
+if __name__ == "__main__":
+    main()
